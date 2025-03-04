65e68591e94d4f88cafaa90ca2db924ddf335c80
Projit Chandra <projitchandra32@gmail.com>
2025-03-04 15:40:25 +0530

    Added VPC peer settings

--- upload-server.py
+++ upload-server.py
@@ -3,7 +3,7 @@ import boto3
 
 prefix_key = "server/"
 local_path = "./server/"
-bucket_name = "consul-bucket-us-east-1-704678691702"
+bucket_name = "consul-bucket-us-east-2-704678691702"
 
 s3_client = boto3.client('s3')
 
